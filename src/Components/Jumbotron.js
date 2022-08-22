6893c17f7b25b38e651199e902c86f2346a685d1
Isaiah Desta <isaiahdesta@Isaiahs-MBP.attlocal.net>
2022-08-22 17:46:43 -0400

    init

---
+++ src/Components/Jumbotron.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function Jumbotron() {
+    return (
+      <div>
+        <div className="jumbotron" >
+          <h1 className="display-4">blah blah</h1>
+          <p className="lead">I am a confounder @ nonsense .co I am a confounder @ nonsense .co I am a confounder @ nonsense .co I am a confounder @ nonsense .co I am a confounder @ nonsense .co</p>
+          <hr className="my-4" />
+          <p>I am a confounder @ nonsense .co I am a confounder @ nonsense .co</p>
+          <a className="btn btn-primary btn-lg" href="#" role="button">elusivate</a>
+        </div>
+      </div>
+    );
+  }
+export default Jumbotron;
