3dd6fe2acfb97c4d1d6a968a480f94ee615cf8fd
Isaiah Desta <isaiahdesta@Isaiahs-MBP.attlocal.net>
2022-08-24 18:10:01 -0400

    attempting to integrate card component.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-
 import Navbar from './Components/Navbar';
 import Jumbotron from './Components/Jumbotron';
 import Card from './Components/Card';
 
 function App() {
   return (
-    <div className = "container-fluid" >
+    <div className="container-fluid">
       <Navbar />
       <Jumbotron />
-      {/* <Card /> */}
+      <Card />
     </div>
   );
 }
